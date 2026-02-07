57d6af31fc35996b6b84379264cf76ac6083626e
Cagliar22 <mdodge2@umbc.edu>
2026-02-07 00:16:55 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -15,9 +15,3 @@ self.addEventListener("activate", e => {
   );
   self.clients.claim();
 });
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
